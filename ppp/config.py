33345d865b6b0623d931febc7a83d5f53334a2a9
JingBo He <2313042320@st.nuc.edu.cn>
2026-09-18 15:54:22 +0800

    Update config.py

--- ppp/config.py
+++ ppp/config.py
@@ -1,1 +1,7 @@
-TOKEN = "4ea92729194a9a3a60f9c75dd60eb9a854622f774030aed003a3e329"
+import os
+TOKEN = '4ea92729194a9a3a60f9c75dd60eb9a854622f774030aed003a3e329'
+START_DATE = '20251212'
+BEFORE_DATE = '20251101'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CSV_PATH = os.path.join(BASE_DIR, '20251212.csv')
+
