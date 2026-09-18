868677d1ea8f0203ca1db88f7becedf5beee6406
JingBo He <2313042320@st.nuc.edu.cn>
2026-09-18 18:00:44 +0800

    Update run2.py

--- ppp/run2.py
+++ ppp/run2.py
@@ -19,23 +19,22 @@ holdings = position.df
 
 quotes = QuoteSource(token, start_date,before_date)
 snapshot = quotes.df
+# print(snapshot)
 
 cons_codes = crawler_simple.get_constituents("000009")  # 爬虫：下载+解析，返回 code 列
-
 cons_codes['code'] = cons_codes['code'].apply(position.add_suffix)
-# print(raw2)
+# print(cons_codes)
+
 pos2 = Portfolio(cons_codes,holdings)
 book1= pos2.df
-# print(df2)
 book1.dropna(inplace=True)
 book1.reset_index(drop=True, inplace=True)
+# print(book1)
 
 pos3 = Portfolio(book1,snapshot)
 book2= pos3.df
-# print(df2)
-
 book2.close = book2.apply(quotes.last_close, axis=1)
-
+# print(book2)
 
 print(f"380中证总市值: {Portfolio.return_market_value(book2):.2f} 元")
 print(f"耗时: {time.perf_counter() - start:.2f} 秒")
