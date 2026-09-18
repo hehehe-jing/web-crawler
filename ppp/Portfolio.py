9b9c390ee3830d5e17e5063e18e42c5d5cecc484
JingBo He <2313042320@st.nuc.edu.cn>
2026-09-18 15:53:18 +0800

    Add static methods for market value and gain calculations

--- ppp/Portfolio.py
+++ ppp/Portfolio.py
@@ -11,7 +11,27 @@ class Portfolio:
         df = pd.merge(df1, df2, on='code', how='left')
         
         self.df = df
+    
+    @staticmethod
+    def return_market_value(book):
+        return (book.hold_vol * book.close).sum()
+    @staticmethod
+    def return_market_value_pre(book):
+        return (book.hold_vol * book.pre_close).sum()
+    @staticmethod
+    def return_gain_by_pre_close(book):
+        return ((book['close'] - book['pre_close']) * book['hold_vol']).sum()
+    @staticmethod
+    def return_gain_by_diff(book):
+        return (book.hold_vol * book.close).sum()-(book.hold_vol * book.pre_close).sum()
+    @staticmethod
+    def return_gain_by_change(book):
+        return (book.hold_vol * book.change).sum()  
+    @staticmethod
+    def return_ret_pct(book):
+        return ((book.hold_vol * book.change).sum())/(book.hold_vol * book.pre_close).sum()*100
 
+    
     
 
 
